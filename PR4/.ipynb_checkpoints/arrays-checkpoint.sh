7ec0d3f52e6961b5cb2007a1c9d6838a3b525d5b
Rassel45 <99266862+Rassel45@users.noreply.github.com>
2023-05-08 20:22:06 +0000

    arrays

--- PR4/.ipynb_checkpoints/arrays-checkpoint.sh
+++ PR4/.ipynb_checkpoints/arrays-checkpoint.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+list=(1 2 3 4 5 6 7 8 9 10)
 Ex1(){
-    nums=("$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}")
-    echo ${nums[@]}
+  str=("$@") # Convertir los parámetros de la función en un array
+  for i in "${str[@]}" #Este for imprime el contenido de la lista
+  do
+    echo "$i"
+  done
 }
-Ex1 1 2 3 4 5 6 7 8 9 10
+Ex1 "${list[@]}"
 
+Ex2(){    
+    # Creamos un array con los archivos del directorio actual
+    files=($(ls))
+    # Mostramos el nombre del primer archivo
+    echo "El primer archivo es: ${files[0]}"
+}
+Ex2
 
 
 Ex3(){
     list=("Hola" "Buenas" "Tardes" "Soy" "Una" "Lista")
-    echo ${#list[@]}
+    echo ${#list[@]} # al poner '#' hace que enseñe cuantos elementos hay en vez de imprimir el elemento en si
 }
 Ex3
 
@@ -17,7 +28,7 @@ Ex4(){
     delete=("hola" "que" "tal" "estoy acoplado")
     echo "antes de borrar"
     echo ${delete[@]}
-    unset delete[$1]
+    unset delete[$1] #unset hace que puedas eliminar un elemento especifico de la lista, si no se especifica posicion elimina la lista entera
     echo "despues de borrar"
     echo ${delete[@]}
 }
@@ -27,16 +38,16 @@ Ex5(){
     add=("hola" "que" "tal")
     echo "antes de añadir"
     echo ${add[@]}
-    add+=("Estoy añadido y no acoplado")
+    add+=("Estoy añadido y no acoplado") #el add añade a la ultima posicion de la lista el contenido que especificamos
     echo "despues de añadir"
     echo ${add[@]}
 }
 Ex5
 
 Ex6(){
-    posant=$(($1-1))
+    posant=$(($1-1)) #esta variable sirve para cuando queramos utilizar la posicion anterior a la que queremos añadir un elemento para editar la lista
     insert=("hola" "tardes")
-    insert=("${insert[$posant]}" "buenas" "${insert[$1]}")
+    insert=("${insert[$posant]}" "buenas" "${insert[$1]}") #aqui cambiamos la lista especificando la posicion donde queremos el nuevo elemento
     echo ${insert[@]}
 }
 Ex6 1
@@ -45,7 +56,7 @@ Ex7(){
     names=("hola" "porfa" "no" "me" "borres" "este de aqui")
     removed=()
     for name in "${names[@]}"; do
-        if [[ $name != "$1" ]]; then
+        if [[ $name != "$1" ]]; then #si este if no detecta el elemento especificado ira añadiendo elementos a otra lista para asi tener una lista borrando lo que queremos
             removed+=("$name")
         fi
     done
@@ -57,7 +68,7 @@ Ex8(){
     names=("hola" "buscas" "a" "este de aqui")
     indice=-1
     for ((i=0; i<${#names[@]}; i++)); do
-        if [[ "${names[i]}" == "$1" ]]; then
+        if [[ "${names[i]}" == "$1" ]]; then #si este if detecta que un elemento de la lista es igual a lo que se especifica en el parametro, imprime que elemento se buscaba y su posicion en la lista
             indice=$i
             echo ${names[$indice]}
             echo "posicion $indice"
@@ -72,7 +83,7 @@ Ex9(){
     listed=("1" "2" "3")
     sum=0
     echo ${listed[@]}
-     for ((i=0; i<${#listed[@]}; i++)); do
+     for ((i=0; i<${#listed[@]}; i++)); do #este for suma todos los elementos de la lista de izquierda a derecha
          sum=$(($sum+${listed[i]}))
     done
     echo $sum
@@ -83,10 +94,10 @@ Ex10(){
     list1=("hola" "que tal")
     list2=("somos" "una lista" "conjunta")
     listogether=()
-    for value in "${list1[@]}"; do
+    for value in "${list1[@]}"; do #este for añade todos los elementos de uno a uno de la list1 a la listogether
         listogether+=("$value")
     done
-    for value in "${list2[@]}"; do
+    for value in "${list2[@]}"; do #este for añade todos los elementos de uno a uno de la list2 a la listogether
         listogether+=("$value")
     done
     echo ${listogether[@]}
